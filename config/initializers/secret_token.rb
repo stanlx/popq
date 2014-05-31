888eeaa8bf783a6744b2f5b5b8c9bed7e969a85c
Stan Melamed <stanmelamed@gmail.com>
2014-05-31 17:35:18 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PathQ::Application.config.secret_key_base = '9e52f5a63dd0099b809021d41274013441d6710a2e860d7291b34dd9c3c654948069a2a6d919d29209eb4473b725b83475eeef31aa407dcdfad7d4b26c84e4f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PathQ::Application.config.session_store :cookie_store, key: '_pathQ_session'
